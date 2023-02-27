a589d95c92de26e8e0c3df20efe06f679b5ffabf
Basil Okache <basilokache@gmail.com>
2023-02-27 10:32:05 +0100

    working on the header

--- wp-content/themes/finals_theme/functions.php
+++ wp-content/themes/finals_theme/functions.php
@@ -49,7 +49,7 @@ function finals_theme_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'menu-1' => esc_html__( 'Primary', 'finals_theme' ),
+			'primary' => esc_html__( 'Primary', 'finals_theme' ),
 		)
 	);
 
--- wp-content/themes/finals_theme/header.php
+++ wp-content/themes/finals_theme/header.php
@@ -26,32 +26,27 @@
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'finals_theme' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$finals_theme_description = get_bloginfo( 'description', 'display' );
-			if ( $finals_theme_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $finals_theme_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+		<div class="nav-menu" id="nav-menu">
+			<div class="site-wrapper">
+				<div class="menu-container" id="menu-container">
+					<div class="site-branding logo-container" id="logo-container">
+						<?php
+							the_custom_logo();
+						?>
+					</div>
+				</div>
+			</div>
+		</div>
 
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'finals_theme' ); ?></button>
 			<?php
 			wp_nav_menu(
 				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
+					'theme_location' 	=> 	'primary-menu',
+					'container'			=>	'div',
+					'container_class'	=>	'',
+					'menu_id'        	=> 	'primary-menu',
 				)
 			);
 			?>
