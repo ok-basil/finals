0cf7efafe6ba21c6b0a6ec3b720afdf50f5ea5cd
Basil Okache <basilokache@gmail.com>
2023-02-28 08:44:45 +0100

    Minor adjustments to the footer

--- wp-content/themes/finals_theme/footer.php
+++ wp-content/themes/finals_theme/footer.php
@@ -16,13 +16,13 @@
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'finals_theme' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'finals_theme' ), 'WordPress' );
+				// printf( esc_html__( 'Proudly powered by %s', 'finals_theme' ), 'WordPress' );
 				?>
 			</a>
-			<span class="sep"> | </span>
+			<!-- <span class="sep"> | </span> -->
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'finals_theme' ), 'finals_theme', '<a href="http://underscores.me/">Basil</a>' );
+				// printf( esc_html__( 'Theme: %1$s by %2$s.', 'finals_theme' ), 'finals_theme', '<a href="http://underscores.me/">Basil</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
