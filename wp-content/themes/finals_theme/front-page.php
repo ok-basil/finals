913da54e6604617fb1feff358b4e06ca32a8a945
Basil Okache <basilokache@gmail.com>
2023-02-28 11:38:12 +0100

    Almost done with the front page

--- wp-content/themes/finals_theme/front-page.php
+++ wp-content/themes/finals_theme/front-page.php
@@ -16,12 +16,98 @@
         </div>
     </section>
 
-    <section>
-
+    <section class="second-section">
+        <div class="site-wrapper">
+            <div class="container">
+                <div class="century-company" id="century-company">
+                    <div class="first-century" id="first-century">
+                        <img src="<?php echo get_template_directory_uri() ?> /assets/img/iphone.png" alt="iphone">
+                    </div>
+                    <div class="second-century" id="second-century">
+                        <h2>Building a 21st century company</h2>
+                        <p>Very experienced software developers take it upon themselves to oversee continued growth of the students during and after the 3 month programme.</p>
+                        <span>
+                            <p>Business</p>
+                            <span>
+                                <p>Read More</p>
+                                <img src="<?php echo get_template_directory_uri() ?> /assets/icons/show-more.png" alt="show-more">
+                            </span>
+                        </span>
+                    </div>
+                </div>
+                <div class="stock-company" id="stock-company">
+                    <div class="first-stock" id="first-stock">
+                       <img src="<?php echo get_template_directory_uri() ?> /assets/img/stock-market.png" alt="stock-market">
+                    </div>
+                    <div class="second-stock" id="second-stock">
+                        <h2>Coindealer lauches on NYSE</h2>
+                        <p>Very experienced software developers take it upon themselves to oversee programme.</p>
+                        <span>
+                            <p>Business</p>
+                            <span>
+                                <p>Read More</p>
+                                <img src="<?php echo get_template_directory_uri() ?> /assets/icons/show-more.png" alt="show-more">
+                            </span>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 
-    <section>
-
+    <section class="third-section">
+        <div class="site-wrapper">
+            <div class="container">
+                <div>
+                    <div class="fibre-network" id="fibre-network">
+                       <img src="<?php echo get_template_directory_uri() ?> /assets/img/fibre.png" alt="fibre">
+                    </div>
+                    <div class="tricycle" id="tricycle">
+                        <h2>Fibre Optic Network in West Africa</h2>
+                        <p>Very experienced software developers take it upon themselves to oversee programme.</p>
+                        <span>
+                            <p>Business</p>
+                            <span>
+                                <p>Read More</p>
+                                <img src="<?php echo get_template_directory_uri() ?> /assets/icons/show-more.png" alt="show-more">
+                            </span>
+                        </span>
+                    </div>
+                </div>
+                <div>
+                    <div class="workstation" id="workstation">
+                       <img src="<?php echo get_template_directory_uri() ?> /assets/img/keke.png" alt="keke">
+                    </div>
+                    <div class="second-stock" id="second-stock">
+                        <h2>Coindealer lauches on NYSE</h2>
+                        <p>Very experienced software developers take it upon themselves to oversee programme.</p>
+                        <span>
+                            <p>Business</p>
+                            <span>
+                                <p>Read More</p>
+                                <img src="<?php echo get_template_directory_uri() ?> /assets/icons/show-more.png" alt="show-more">
+                            </span>
+                        </span>
+                    </div>
+                </div>
+                <div>
+                    <div class="first-stock" id="first-stock">
+                       <img src="<?php echo get_template_directory_uri() ?> /assets/img/workstation.png" alt="workstation">
+                    </div>
+                    <div class="second-stock" id="second-stock">
+                        <h2>Coindealer lauches on NYSE</h2>
+                        <p>Very experienced software developers take it upon themselves to oversee programme.</p>
+                        <span>
+                            <p>Business</p>
+                            <span>
+                                <p>Read More</p>
+                                <img src="<?php echo get_template_directory_uri() ?> /assets/icons/show-more.png" alt="show-more">
+                            </span>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 </main>
 
