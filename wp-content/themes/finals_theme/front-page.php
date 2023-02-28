46063a2d14b19b4b3de42b6ca4dfe58ee86cddfb
Basil Okache <basilokache@gmail.com>
2023-02-28 09:47:17 +0100

    First section has been done

--- wp-content/themes/finals_theme/front-page.php
+++ wp-content/themes/finals_theme/front-page.php
@@ -1,7 +1,28 @@
 <?php get_header() ?>
 
 <main>
-    
+    <section class="first-section">
+        <div class="site-wrapper">
+            <div class="container">
+                <h1>Together as partners we've built better. See what we're up to.</h1>
+                <div class="filter-posts" id="filter-posts">
+                    <p><strong>Filter Posts:</strong></p>
+                    <span>
+                        <p>All Categories</p>
+                        <p><img src="<?php echo get_template_directory_uri() ?> /assets/icons/dropdown.png" alt="dropdown"></p>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+
+    </section>
+
+    <section>
+
+    </section>
 </main>
 
 <?php get_footer() ?>
