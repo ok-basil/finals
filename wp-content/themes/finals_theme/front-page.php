3f7be1379da59e760f80bb25240e25384cbce73a
Basil Okache <basilokache@gmail.com>
2023-03-01 17:46:24 +0100

    Minor adjustments to the front page

--- wp-content/themes/finals_theme/front-page.php
+++ wp-content/themes/finals_theme/front-page.php
@@ -58,23 +58,23 @@
     <section class="third-section">
         <div class="site-wrapper">
             <div class="container">
-                <div class="fibre-keke-workstation" id="fibre-keke-workstation">
-                    <div class="first-part" id="first-part">
-                       <img src="<?php echo get_template_directory_uri() ?> /assets/img/fibre.png" alt="fibre">
-                    </div>
-                    <div class="second-part" id="second-part">
-                        <h2 class="margin-remover">Fibre Optic Network in West Africa</h2>
-                        <p class="margin-remover">Very experienced software developers take it upon themselves to oversee programme.</p>
-                        <span>
-                            <p>Business</p>
+                    <div class="fibre-keke-workstation" id="fibre-keke-workstation">
+                        <div class="first-part" id="first-part">
+                        <img src="<?php echo get_template_directory_uri() ?> /assets/img/fibre.png" alt="fibre">
+                        </div>
+                        <div class="second-part" id="second-part">
+                            <h2 class="margin-remover">Fibre Optic Network in West Africa</h2>
+                            <p class="margin-remover">Very experienced software developers take it upon themselves to oversee programme.</p>
                             <span>
-                                <p>Read More</p>
-                                <img src="<?php echo get_template_directory_uri() ?> /assets/icons/show-more.png" alt="show-more">
+                                <p>Business</p>
+                                <span>
+                                    <p>Read More</p>
+                                    <img src="<?php echo get_template_directory_uri() ?> /assets/icons/show-more.png" alt="show-more">
+                                </span>
                             </span>
-                        </span>
+                        </div>
                     </div>
-                </div>
-                <div class="fibre-keke-workstation" id="fibre-keke-workstation">
+                    <div class="fibre-keke-workstation" id="fibre-keke-workstation">
                     <div class="first-part" id="first-part">
                        <img src="<?php echo get_template_directory_uri() ?> /assets/img/keke.png" alt="keke">
                     </div>
