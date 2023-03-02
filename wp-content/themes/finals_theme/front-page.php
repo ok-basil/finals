a92c163e03f1b02b9938555cec429e2ea2ad4a98
Basil Okache <basilokache@gmail.com>
2023-03-02 21:36:28 +0100

    Working on the post page

--- wp-content/themes/finals_theme/front-page.php
+++ wp-content/themes/finals_theme/front-page.php
@@ -1,4 +1,14 @@
 <?php 
+
+/**
+ * Template name: Front Page
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package finals_theme
+ */
+
     get_header(); 
     $post_id = get_the_ID();
 ?>
@@ -23,86 +33,45 @@
         <div class="site-wrapper">
             <div class="container">
                 <div class="blog-posts-wrapper">
-                    <div class="post-style" id="post-style">
-                        <div class="first" id="first">
-                            <img src="<?php echo get_template_directory_uri() ?> /assets/img/iphone.png" alt="iphone">
-                        </div>
-                        <div class="second" id="second">
-                            <h2>Building a 21st century company</h2>
-                            <p>Very experienced software developers take it upon themselves to oversee continued growth of the students during and after the 3 month programme.</p>
-                            <span>
-                                <p>Business</p>
-                                <span>
-                                    <p>Read More</p>
-                                    <img src="<?php echo get_template_directory_uri() ?> /assets/icons/show-more.png" alt="show-more">
-                                </span>
-                            </span>
-                        </div>
-                    </div>
-                    <div class="post-style" id="post-style">
-                        <div class="first" id="first">
-                            <img src="<?php echo get_template_directory_uri() ?> /assets/img/stock-market.png" alt="stock-market">
-                        </div>
-                        <div class="second" id="second">
-                            <h2>Coindealer launches on NYSE</h2>
-                            <p>Very experienced software developers take it upon themselves to oversee programme.</p>
-                            <span>
-                                <p>Business</p>
-                                <span>
-                                    <p>Read More</p>
-                                    <img src="<?php echo get_template_directory_uri() ?> /assets/icons/show-more.png" alt="show-more">
-                                </span>
-                            </span>
-                        </div>
-                    </div>
-                    <div class="post-style" id="post-style">
-                        <div class="first" id="first">
-                            <img src="<?php echo get_template_directory_uri() ?> /assets/img/fibre.png" alt="fibre">
-                        </div>
-                        <div class="second" id="second">
-                            <h2>Fibre Optic Network in West Africa</h2>
-                            <p>Very experienced software developers take it upon themselves to oversee programme.</p>
-                            <span>
-                                <p>Business</p>
-                                <span>
-                                    <p>Read More</p>
-                                    <img src="<?php echo get_template_directory_uri() ?> /assets/icons/show-more.png" alt="show-more">
-                                </span>
-                            </span>
-                        </div>
-                    </div>
-                    <div class="post-style" id="post-style">
-                        <div class="first" id="first">
-                            <img src="<?php echo get_template_directory_uri() ?> /assets/img/keke.png" alt="keke">
-                        </div>
-                        <div class="second" id="second">
-                            <h2>Coindealer lauches on NYSE</h2>
-                            <p>Very experienced software developers take it upon themselves to oversee programme.</p>
-                            <span>
-                                <p>Business</p>
-                                <span>
-                                    <p>Read More</p>
-                                    <img src="<?php echo get_template_directory_uri() ?> /assets/icons/show-more.png" alt="show-more">
-                                </span>
-                            </span>
-                        </div>
-                    </div>
-                    <div class="post-style" id="post-style">
-                        <div class="first" id="first">
-                            <img src="<?php echo get_template_directory_uri() ?> /assets/img/workstation.png" alt="stock-market">
-                        </div>
-                        <div class="second" id="second">
-                            <h2>Coindealer lauches on NYSE</h2>
-                            <p>Very experienced software developers take it upon themselves to oversee programme.</p>
-                            <span>
-                                <p>Business</p>
-                                <span>
-                                    <p>Read More</p>
-                                    <img src="<?php echo get_template_directory_uri() ?> /assets/icons/show-more.png" alt="show-more">
-                                </span>
-                            </span>
-                        </div>
-                    </div>
+                    <?php $args = [
+                        'post_type'     => 'post',
+                        'post_per_page' => 5,
+                    ];
+
+                    $the_query = new \WP_Query($args);
+                    $post = the_post();
+
+                    if($the_query->have_posts()){
+                        $count = 0;
+                        while($the_query->have_posts()) {
+                            $the_query->the_post();
+                            $post_id = get_the_ID();
+                            $the_post = get_post($post_id);
+                            $post_content = strip_tags($the_post->post_content);
+                            $count ++;
+                            ?>
+                            <div class="post-style" id="post-style">
+                                <div class="first" id="first">
+                                    <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="" >
+                                </div>
+                                <div class="second" id="second">
+                                    <h2><?php echo get_the_title(); ?></h2>
+                                    <p> <?php if($count === 1){
+                                        echo truncate($post_content, 140);
+                                        } else {?>
+                                        <?php echo truncate($post_content, 90); }?>
+                                    </p>
+                                    <span>
+                                        <p>Business</p>
+                                        <span>
+                                            <p>Read More</p>
+                                            <img src="<?php echo get_template_directory_uri() ?> /assets/icons/show-more.png" alt="show-more">
+                                        </span>
+                                    </span>
+                                </div>
+                            </div>
+                        <?php }
+                    } ?>
                 </div>
             </div>
         </div>
