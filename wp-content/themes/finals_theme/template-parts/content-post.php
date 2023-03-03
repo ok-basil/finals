f053dbf923ec7207f05c6e723d8b0e4674f2c320
Basil Okache <basilokache@gmail.com>
2023-03-03 12:50:28 +0100

    styling single post page

--- wp-content/themes/finals_theme/template-parts/content-post.php
+++ wp-content/themes/finals_theme/template-parts/content-post.php
@@ -7,9 +7,9 @@
  */
 ?>
 
-<section class="blog-post-header" id="blog-post-header" style="background-image: url('<?php echo get_the_post_thumbnail_url(); ?>"> 
+<section class="blog-post-header" id="blog-post-header" style="background-image: url('<?php echo get_the_post_thumbnail_url(); ?>')"> 
     <div class="site-wrapper">
-        <div>
+        <div class="the-title" id="the-title">
             <h1><?php echo get_the_title(); ?></h1>
         </div>
     </div>
