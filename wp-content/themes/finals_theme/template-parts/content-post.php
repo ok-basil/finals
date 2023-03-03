047c41bb775c33a96d1dee12fc61dd72cfc1c544
Basil Okache <basilokache@gmail.com>
2023-03-03 08:50:55 +0100

    single post

--- wp-content/themes/finals_theme/template-parts/content-post.php
+++ wp-content/themes/finals_theme/template-parts/content-post.php
@@ -1,3 +1,32 @@
-<section class="blog-post-header" id="blog-post-header" style="background-image: url('<?php echo get_the_post_thumbnail_url(); ?>">
+<?php 
 
+/**
+ * Template part for displaying posts
+ * 
+ * @package finals_theme
+ */
+?>
+
+<section class="blog-post-header" id="blog-post-header" style="background-image: url('<?php echo get_the_post_thumbnail_url(); ?>"> 
+    <div class="site-wrapper">
+        <div>
+            <h1><?php echo get_the_title(); ?></h1>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="site-wrapper">
+        <div class="mid-section" id="mid-section">
+            <div>
+                <span>
+                    <span>
+                        
+                    </span>
+                    <p>Product development</p>
+                </span>
+            </div>
+            <div></div>
+        </div>
+    </div>
 </section>
